670329602de1a85770075b893c747e1f6eddb42c
Buddhika Weerasekara <wmbpbw@gmail.com>
2025-03-16 15:21:22 +0530

    docker configs

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js';
 
 // Keycloak initialization options
 const keycloakConfig = {
-    url: process.env.REACT_APP_KEYCLOAK_URL || 'http://keycloak:8080',
+    url: process.env.REACT_APP_KEYCLOAK_URL || 'http://localhost:8080',
     realm: process.env.REACT_APP_KEYCLOAK_REALM || 'web-analyzer',
     clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || 'web-analyzer-frontend'
 };
